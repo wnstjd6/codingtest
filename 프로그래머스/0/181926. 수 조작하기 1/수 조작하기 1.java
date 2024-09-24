f1f5f2bd9911425dbc1f4a88905a686241feb79d
wnstjd6 <165138560+wnstjd6@users.noreply.github.com>
2024-09-24 11:12:02 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.16 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        for(int i = 0; i<control.length(); i++){
+           char a = control.charAt(i);
+            if(a == 'w'){
+                n+=1;
+            }
+            else if(a=='s'){
+                n-=1;
+            }
+            else if(a=='d'){
+                n+=10;
+            }
+            else if(a=='a'){
+                n-=10;
+            }
+        }
+       
+        return n;
+    }
+}
