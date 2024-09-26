345b30bc2f34b630146def422bbba8f4dbac3e69
wnstjd6 <165138560+wnstjd6@users.noreply.github.com>
2024-09-26 18:44:55 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        int sum = 0;
+        for(int i = 1; i<=t; i++){
+            n *= 2;
+        }
+        answer = n;
+        return answer;
+    }
+}
