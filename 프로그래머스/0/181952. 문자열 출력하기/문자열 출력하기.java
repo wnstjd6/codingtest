9faaf4e6b158af89492408cfa6a3d62be9d00c2d
junseng <165138560+wnstjd6@users.noreply.github.com>
2024-10-08 03:12:40 +0900

    [level 0] Title: 문자열 출력하기, Time: 153.94 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -4,7 +4,6 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        
         System.out.println(a);
     }
 }
